780535d57a8c6bac9ef6637e8d960fe2bcaf889b
theoverse <theoverse@protonmail.com>
2022-02-27 20:42:10 -0600

    rendered ejs templates and set handler for static files

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 const express = require('express');
 const app = express();
 
-let hostname = '127.0.0.1'
-let port = 3000
+const bicycles = require('./data/data.json');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+let hostname = '127.0.0.1';
+let port = 3000;
 
 app.get('/', (req, res) => {
-    return res.send(`Server is running on ${hostname} at port ${port}`)
+    return res.render('bicycles')
+});
+
+app.get('/bicycle', (req, res) => {
+    // console.log(req.query.id)
+    const bicycle = bicycles.find(b => b.id === req.query.id)
+    return res.render('overview');
 });
 
 app.listen(3000, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
