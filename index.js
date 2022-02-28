542cef20c74c6ee06dd81b31c06643ca742dc6f0
theoverse <theoverse@protonmail.com>
2022-02-27 21:21:01 -0600

    added dynamic data rendering on the overview page

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ const app = express();
 const bicycles = require('./data/data.json');
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 let hostname = '127.0.0.1';
 let port = 3000;
 
 app.get('/', (req, res) => {
-    return res.render('bicycles')
+    return res.render('bicycles');
 });
 
 app.get('/bicycle', (req, res) => {
-    // console.log(req.query.id)
-    const bicycle = bicycles.find(b => b.id === req.query.id)
-    return res.render('overview');
+
+    const bicycle = bicycles.find(b => b.id === req.query.id);
+    console.log(bicycle)
+    return res.render('overview', {
+        bicycle
+    });
 });
 
 app.listen(3000, () => {
